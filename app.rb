87d25e525385ecadf8fb579c58edfab71f3dd639
YanC Lin <yonalin1@gmail.com>
2018-03-09 19:49:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/:name' do
+  get '/name' do
     "My name is #{params['name']}"
   end
-  get '/:hometown' do
+  get '/hometown' do
      "My hometown is #{params['hometown']}"
    end
-   get '/:song' do
-     "My favorite song is #{params['song']}"
+   get '/favorite-song' do
+     "My favorite song is 'Hotline Bling'"
   end
 
 end
